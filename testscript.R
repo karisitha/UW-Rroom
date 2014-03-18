a996600d7347a0e91fc3d41d60c82d1d2a371576
Jonathan Kocarnik <jkocarni@fhcrc.org>
2014-03-18 12:19:50 -0700

    added code for abplot

--- testscript.R
+++ testscript.R
@@ -5,3 +5,33 @@
 ## testing adding comments as a collaborator.
 
 ## another test of adding a comment.
+
+
+
+## defining variables for use 
+a <- 2
+b <- -3
+sigSq <- 0.5
+x <- runif(40)
+y <- a + b*x + rnorm(40,sd = sqrt(sigSq))
+
+## plot the line
+plot(x,y)
+
+## put a line through the plot
+abline(a,b, col="red")
+
+
+## can use export feature to save an image of the plot (e.g. a .png file)
+## but this isn't recommended because isn't exactly reproducible (not embedded in
+## the code).  Also slower (as manually need to save after changes), particularly
+## if running more than one plot.
+
+## instead, can have a command to save image for you (here a pdf), so anyone who 
+## runs the script gets the same plot as you, and is reproducible (as a bonus,
+## may also be able to find this R code in search features)
+dev.print(pdf, "toyline plot.pdf")
+
+## if putting things online, good to have *.png formats
+dev.print(png, "toylinePlot.png", width=480)
+
