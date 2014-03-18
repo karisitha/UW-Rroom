ef02a4e734eab79a89d295be10d3781f5b132858
Anna Batorsky <karisitha45@yahoo.com>
2014-03-18 12:27:24 -0700

    i'm a little confused

---
+++ toyline.R
@@ -0,0 +1,9 @@
+a <- 2
+b <- -3
+sigSq <- 0.5
+x <- runif(40)
+y <- a + b*x + rnorm(40, sd = sqrt(sigSq))
+plot(x,y)
+abline(a,b, col="green")
+dev.print(png, "toylinePlot.png", width=480)
+dev.print(pdf, "toylinePlot.pdf")
